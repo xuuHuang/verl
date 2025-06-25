eda07d223666685b41f49bbdf059f5ccfd4fbe9d
Xu Huang <41568010+xuuHuang@users.noreply.github.com>
2025-06-25 18:51:36 +0800

    fix: fix the type of parquet_files
    Fix the type of parquet_files in sft_dataset.py. When sending a list of files, the type of parquet_files is ListConfig, not List[str].

--- verl/utils/dataset/sft_dataset.py
+++ verl/utils/dataset/sft_dataset.py
@@ -23,6 +23,7 @@ from typing import List, Union
 import pandas as pd
 import torch
 from torch.utils.data import Dataset
+from omegaconf.listconfig import ListConfig
 from transformers import PreTrainedTokenizer
 
 from verl.utils import hf_tokenizer
@@ -38,7 +39,7 @@ class SFTDataset(Dataset):
         config (OmegaConf): the data config
     """
 
-    def __init__(self, parquet_files: Union[str, List[str]], tokenizer, config):
+    def __init__(self, parquet_files: Union[str, ListConfig], tokenizer, config):
         prompt_key = config.get("prompt_key", "prompt")
         prompt_dict_keys = config.get("prompt_dict_keys", None)
         response_key = config.get("response_key", "response")
@@ -51,7 +52,7 @@ class SFTDataset(Dataset):
         self.truncation = truncation
         self.use_shm = use_shm
 
-        if not isinstance(parquet_files, List):
+        if not isinstance(parquet_files, ListConfig):
             parquet_files = [parquet_files]
 
         self.parquet_files = parquet_files
